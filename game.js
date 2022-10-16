cf019f741e6569b63e5d3f2c90977d7099b52b5f
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-10-16 18:53:16 +0200

    entire day spent on one fuc$ bug

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const question = document.querySelector("#question");
-const choices = Array.from(document.querySelectorAll(".choices-text"));
+const choices = Array.from(document.querySelectorAll(".choice-text"));
 const progressText = document.querySelector("#progressText");
 const scoreText = document.querySelector("#score");
 const progressBarFull = document.querySelector("#progressBarFull");
@@ -16,6 +16,22 @@ let questions = [
     choice2: "Scripting Language",
     choice3: "Markup Language",
     choice4: "Programming Language",
+    answer: 1,
+  },
+  {
+    question: "what is 2+2?",
+    choice1: "2",
+    choice2: "4",
+    choice3: "23",
+    choice4: "20",
+    answer: 2,
+  },
+  {
+    question: "what is 2+2?",
+    choice1: "2",
+    choice2: "4",
+    choice3: "23",
+    choice4: "20",
     answer: 3,
   },
   {
@@ -24,27 +40,59 @@ let questions = [
     choice2: "4",
     choice3: "23",
     choice4: "20",
-    answer: 2,
-  },
-  {
-    question: "what is 2+2?",
-    choice1: "2",
-    choice2: "4",
-    choice3: "23",
-    choice4: "20",
-    answer: 2,
-  },
-  {
-    question: "what is 2+2?",
-    choice1: "2",
-    choice2: "4",
-    choice3: "23",
-    choice4: "20",
-    answer: 2,
+    answer: 4,
   },
 ];
 const SCORE_POINTS = 100;
 const MAX_QUESTIONS = 4;
 startGame = () => {
   questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
 };
+getNewQuestion = () => {
+  if ((availableQuestions, length === 0 || questionsCounter > MAX_QUESTIONS)) {
+    localStorage.setItem("mostRecentScore", score);
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionsIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionsIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    let classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(SCORE_POINTS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
